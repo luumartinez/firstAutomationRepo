c1550bd3ad08499735e5177feba369c4b3849fe7
luumartinez <luuumartinez13@gmail.com>
2024-03-05 19:43:41 -0300

    changes in yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'q967d3',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
